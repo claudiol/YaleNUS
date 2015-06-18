309034207d3566ee436193cb4b7329e8ddcf1e47
Jenkins <jenkins@rhic-jenkins.iad.salab.redhat.com>
2015-06-17 19:35:34 -0600

    New CloudFlareDNS Version 0.2 Changes to be committed: new file:   CloudFlareDNS-0.2.gem modified:   Makefile Added install-0.2 and uninstall-0.2 rules. These will install/uninstall the 0.2 version of the CloudFlareDNS gem. new file:   dist/CloudFlareDNS-0.1.gem new file:   dist/CloudFlareDNS-0.2.gem Added a new dist subdirectory to hold new versions of the gem. modified:   lib/CloudFlareDNS.rb Added functionality to pass options to the gem methods modified:   spec/CloudFlare.gemspec new file:   test/test-cloudflare-0-2.rb New test suite for the new gem version modified:   test/test-cloudflare.rb

--- Integration/CloudFlare/lib/CloudFlareDNS.rb
+++ Integration/CloudFlare/lib/CloudFlareDNS.rb
@@ -40,28 +40,20 @@ class CloudFlareDNS
       @host = @config['credentials']['apihost']
     else
       # We will get each option from the options hash
-      if options.has_key?(:tkn)
-         @tkn = options[:tkn]
-      else
-         raise "Need :tkn value for CloudFlare credentials"
+      if options.has_key?(:tkn) == false || 
+	 options.has_key?(:email) == false ||
+         options.has_key?(:zoneid) == false ||
+         options.has_key?(:apihost) == false 
+	raise "Please pass all required options :tkn, :email, :zoneid, :apihost"
       end
-      if options.has_key?(:email)
-        @email = options[:email]
-      else
-        raise "Need :email value for CloudFlare credentials"
-      end
-      if options.has_key?(:zoneid)
-        @zone  = options[:zoneid]
-      else
-        raise "Need :zoneid value to execute to CloudFlare API "
-      end
-      if options.has_key?(:apihost)
-        # Set the Cloudflare host
-        @host = options[:apihost]
-      else
-        raise "Need :apihost value for CloudFlare credentials"
-      end
+	 
+      @tkn = options[:tkn]
+      @email = options[:email]
+      @zone  = options[:zoneid]
+      # Set the Cloudflare host
+      @host = options[:apihost]
     end
+
     # get the Cloudflare Instance - Singleton
     @client = CloudFlareConnection.instance
   end
@@ -84,10 +76,35 @@ class CloudFlareDNS
   # 
   # @!method add_cloudflare_record
   #
-  # @param json_data - Data paylod for API.
+  # @param options - Options Hash with the following required parameters:
+  #  :type 
+  #  A 	 RFC 1035[1] 	IPv4 Address record 
+  #  AAAA RFC 3596[2] 	IPv6 address record
+  #  CNAME RFC 1035[1] 	Canonical name record
+  #  LOC RFC 1876 	Location record
+  #  MX 	RFC 1035[1] 	Mail exchange record
+  #  NS 	RFC 1035[1] 	Name server record
+  #  TXT RFC 1035[1] 	Text record
+  #
+  #  :name     
+  #  FQDN for host e.g. "host.example.com"
+  #
+  #  :content
+  #  IP address for host e.g. 192.168.0.1
+  #
+  #  Optional:
+  #  :ttl 
+  #  Time to live for DNS record. Value of 1 is 'automatic'
   #
   # @return [Hash] Returns the REST API Response from CloudFlareDNS
-  def add_cloudflare_record(json_data)
+  def add_cloudflare_record(options)
+
+    # Check required options
+    if options.has_key?(:type) == false || options.has_key?(:name) == false || options.has_key?(:content) == false
+	raise "Required options :type, :name and :content need to be present"
+    end
+
+    json_data = JSON.generate(options)
 
     # Add a record to Cloudflare 
 
@@ -104,8 +121,7 @@ class CloudFlareDNS
     begin
        response = @client.post(@location, json_data)
     rescue => ex
-       puts ex.message
-       puts ex.response
+       raise ex
     end
 
     response
@@ -114,17 +130,56 @@ class CloudFlareDNS
   # 
   # @!method list_cloudflare_dns_records
   # 
-  # With this method you can list one or more records depending on the request.
-  # The request payload to CloudFlare would look something like this:
-  # type=A&name=example.com&content=127.0.0.1&page=1&per_page=20&order=type&direction=desc&match=all
+  #  With this method you can list one or more records depending on the request.
+  #  The request payload to CloudFlare would look something like this:
+  #  type=A&name=example.com&content=127.0.0.1&page=1&per_page=20&order=type&direction=desc&match=all
   # 
-  # @param request - Data request payload for API.
+  # @param options - Options Hash with one of the following parameters:
+  #
+  #  :type 
+  #  A 	 RFC 1035[1] 	IPv4 Address record 
+  #  AAAA RFC 3596[2] 	IPv6 address record
+  #  CNAME RFC 1035[1] 	Canonical name record
+  #  LOC RFC 1876 	Location record
+  #  MX 	RFC 1035[1] 	Mail exchange record
+  #  NS 	RFC 1035[1] 	Name server record
+  #  TXT RFC 1035[1] 	Text record
+  #
+  #  :name     
+  #  FQDN for host e.g. "host.example.com"
+  #
+  #  :content
+  #  IP address for host e.g. 192.168.0.1
+  #
+  #  :match
+  #  all - Match all options (AND)
+  #  any - Match any options (OR)
   #
   # @return [Hash] Returns response from CloudFlare REST API
 
-  def list_cloudflare_dns_record(request)
+  def list_cloudflare_dns_record(options)
 
-    # list records from Cloudflare 
+    if options[:type].empty? == false
+	request = "type=#{options[:type]}&"
+    else
+	request = "type=A&"
+    end
+
+    if options[:name].empty? == false
+       request += "name=#{options[:name]}&"
+    end
+
+    if options[:content].empty? == false
+       request += "content=#{options[:content]}&"
+    end
+
+    if options[:match].empty? == false
+       request += "match=#{options[:match]}"
+    else
+	request += "match=all"
+    end
+
+    # list records from CloudFlare 
 
     # get the Cloudflare Instance - Singleton
     @client = CloudFlareConnection.instance
@@ -139,8 +194,7 @@ class CloudFlareDNS
     begin
        response = @client.get(@location, nil)
     rescue => ex
-       puts ex.message
-       puts ex.backtrace
+       raise ex
     end
 
     return response
@@ -149,25 +203,29 @@ class CloudFlareDNS
   # 
   # @!method delete_cloudflare_dns_record
   # 
-  # With this method you can delete a record depending on the request.
-  # The request payload to CloudFlare would look something like this:
-  # Example request looks like this:
-  # c04e7e5094eb510a7241c30688fcbf0b
+  #  With this method you can delete a record depending on the request.
+  #  The request payload to CloudFlare would look something like this:
+  #  Example request looks like this:
+  #  c04e7e5094eb510a7241c30688fcbf0b
   # 
-  # Basically we pass in the ID for the DNS record and append it to the REST API call.
-  # Example: /client/v4/zones/:zone/dns_records/" + request
+  #  Basically we pass in the ID for the DNS record and append it to the REST API call.
+  #  Example: /client/v4/zones/:zone/dns_records/" + request
   #
-  # @param request - Data request payload for API.
+  # @param dns_id - DNS record ID 
   #
   # @return [Hash] Returns the response from the REST API
 
-  def delete_cloudflare_dns_record(request)
+  def delete_cloudflare_dns_record(dns_id)
+
+    if dns_id.empty?
+    	raise "dns_id cannot be empty"
+    end
 
     # get the Cloudflare Instance - Singleton
     @client = CloudFlareConnection.instance
 
     # Set the location ... 
-    @location = "/client/v4/zones/#{@zone}/dns_records/" + request
+    @location = "/client/v4/zones/#{@zone}/dns_records/" + dns_id 
 
     @client.setup(@tkn, @email, @host)
 
@@ -176,8 +234,7 @@ class CloudFlareDNS
     begin
        response = @client.delete(@location, nil)
     rescue => ex
-       puts ex.message
-       puts ex.backtrace
+       raise ex
     end
 
     return response
@@ -201,8 +258,7 @@ class CloudFlareDNS
     begin
        response = @client.get(@location, nil)
     rescue => ex
-       puts ex.message
-       puts ex.backtrace
+       raise ex
     end
 
     return response
@@ -211,7 +267,7 @@ class CloudFlareDNS
 
   # 
   # @!method list_all_dns_records
-  # The CloudFlare API returns up to 100 records per page.  We request the maximum number of records.
+  #  The CloudFlare API returns up to 100 records per page.  We request the maximum number of records.
   # 
   #
   #  #Lets use the CloudFlareDNS to get DNS records ...     
@@ -257,8 +313,7 @@ class CloudFlareDNS
     begin
        response = @client.get(@location, nil)
     rescue => ex
-       puts ex.message
-       puts ex.backtrace
+       raise ex
     end
 
     return response
--- Integration/CloudFlare/spec/CloudFlare.gemspec
+++ Integration/CloudFlare/spec/CloudFlare.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name        = 'CloudFlareDNS'
-  s.version     = '0.1'
-  s.date        = '2015-03-13'
+  s.version     = '0.2'
+  s.date        = '2015-06-17'
   s.summary     = "CloudFlare DNS Gem"
   s.description = "CloudFlare GEM to support add/delete/list DNS records"
   s.authors     = ["Lester Claudio"]
